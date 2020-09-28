8329a270069d38a8be215dffcfe6d7020fae20b2
KhurshedUlugov <kh.ulugov@gmail.com>
2020-09-28 17:16:31 +0500

    v2

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -3,9 +3,19 @@ package types
 type Money int64
 // Category категории
 type Category string
+
+type Status string
+
+const (
+	StatusOk Status = "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
 // Payment информация о платеже
 type Payment struct {
 	ID int
 	Amount Money
 	Category Category
+	Status Status
 }
